f74a63deee9dcc929c676c68a9d99a7c95dff36c
ASArutyunyan <aleks_pochta_krd@mail.ru>
2025-01-16 14:59:51 +0300

    added before each, after each

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,17 +1,34 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class First {
-    String SITE_URL = "https://mvnrepository.com/";
+    String SITE_URL = "https://mail.ru/";
     WebDriver driver;
+
+    @BeforeEach
+    public void start(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    public void finish(){
+        driver.quit();
+    }
 
     @Test
     public void firstTest(){
-
+        driver.get(SITE_URL);
     }
 
     @Test
     public void secondTest(){
+        driver.get(SITE_URL);
 
     }
 }
