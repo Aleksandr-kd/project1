7620699be7672ea3a4ebee0dd795a639cfb000ac
ASArutyunyan <aleks_pochta_krd@mail.ru>
2025-01-16 14:50:56 +0300

    added pv

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,3 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
 public class First {
     String SITE_URL = "https://mvnrepository.com/";
+    WebDriver driver;
+
+    @Test
+    public void firstTest(){
+
+    }
+
+    @Test
+    public void secondTest(){
+
+    }
 }
