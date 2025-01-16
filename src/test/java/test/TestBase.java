2bd7e53465634cbab19e5f0335172732148070e0
ASArutyunyan <aleks_pochta_krd@mail.ru>
2025-01-16 15:38:40 +0300

    added abstract methods

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,10 +1,10 @@
+package test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
